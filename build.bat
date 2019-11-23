8fe87c459c7332c3bb23fab53380e8da5d57fcbe
Henrique Misael <henrique.nolear@gmail.com>
2019-11-23 15:30:56 -0300

    Renamed flex file to "tokens"

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 del calclang.exe
 bison -d parser.y
-flex calclang.l
+flex tokens.l
 gcc -o calclang symbols.h symbols.c parser.tab.h parser.tab.c lex.yy.c -lfl -lm -w
 del parser.tab.h parser.tab.c lex.yy.c
